13328b7d8208420366663861ad45c9308f3175ad
mihee <algml9603@gmail.com>
2022-06-28 03:58:52 +0900

    feat: 사용자 정보 넣어주기

--- nest_project/src/auth/auth.controller.ts
+++ nest_project/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -15,5 +16,17 @@ export class AuthController {
     @Post('/signin')
     signIn(@Body(ValidationPipe) AuthCredentialsDto: AuthCredentialsDto ): Promise<{accessToken:string}> {
         return this.authService.signIn(AuthCredentialsDto)
+    }
+
+    @Post('/test')
+    test(@Req() req) {
+        console.log('req===', req)
+    }
+
+    @Post('/authTest')
+    @UseGuards(AuthGuard())
+    // UseGuards안에 @nestjs/passport에서 가져온 AuthGuard()를 이용하여 요청안에 유저 정보를 넣어줄 수 있다.
+    authTest(@Req() req) {
+        console.log(req);
     }
 }
